c79207ccd7bfeeee9815722542bd656c8006b004
Lil_Purple_Toti <arthur.ergin.16@gmail.com>
2024-05-14 13:23:02 +0200

    items, champions, runes and sums handled

--- main.go
+++ main.go
@@ -43,6 +43,9 @@ type _LoLDataLinks struct {
 	LanguagesLink      []string `json:"languages"`
 	VersionLink        []string `json:"version"`
 	ItemsLink          []string `json:"items"`
+	ChampionsLink      []string `json:"champions"`
+	SummonersLink      []string `json:"summoners"`
+	RunesLink          []string `json:"runes"`
 	ItemsFullImageLink []string `json:"itemfullimage"`
 }
 
@@ -60,9 +63,10 @@ func _checkVersionLoading() {
 	}
 }
 
-func checkLoadings() {
+func checkLoadings() any {
 	_checkLinksLoading()
 	_checkVersionLoading()
+	return nil
 }
 
 var lolDataLinks _LoLDataLinks = _LoLDataLinks{}
@@ -71,12 +75,15 @@ var loadDataLinksError error = loadFileInStruct("./loldatalinks.json", &lolDataL
 var lolDataVersion string
 var loadDataVersionError error = loadFileInStruct(generateLink(lolDataLinks.VersionLink, []string{}), &lolDataVersion)
 
+// kinda cursed ^^
+var _ any = checkLoadings()
+
 // handling data
 
 type LoLLanguages []string
 
 type LoLItems struct {
-	Data map[string]LoLItem
+	Data map[string]LoLItem `json:"data"`
 }
 
 type LoLItem struct {
@@ -84,6 +91,39 @@ type LoLItem struct {
 	Image struct {
 		Full string `json:"full"`
 	} `json:"image"`
+}
+
+type LoLChampions struct {
+	Data map[string]LoLChampion `json:"data"`
+}
+
+type LoLChampion struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
+}
+
+type LoLSummoners struct {
+	Data map[string]LoLSummoner `json:"data"`
+}
+
+type LoLSummoner struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
+}
+
+type LoLRunes []LoLRuneTree
+
+type LoLRuneTree struct {
+	Name  string       `json:"name"`
+	Slots []LoLRuneRow `json:"slots"`
+}
+
+type LoLRuneRow struct {
+	Runes []LoLRune `json:"runes"`
+}
+
+type LoLRune struct {
+	Name string `json:"name"`
 }
 
 func getLoLLanguages() LoLLanguages {
@@ -110,28 +150,64 @@ func getLoLItems(language string) LoLItems {
 	return items
 }
 
+func getLoLChampions(language string) LoLChampions {
+	var champs LoLChampions = LoLChampions{}
+	err := loadFileInStruct(generateLink(lolDataLinks.ChampionsLink, []string{lolDataVersion, language}), &champs)
+	if err != nil {
+		fmt.Println(err.Error())
+		panic("Error in loading Champions")
+	}
+	return champs
+}
+
+func getLoLSummoners(language string) LoLSummoners {
+	var summs LoLSummoners = LoLSummoners{}
+	err := loadFileInStruct(generateLink(lolDataLinks.SummonersLink, []string{lolDataVersion, language}), &summs)
+	if err != nil {
+		fmt.Println(err.Error())
+		panic("Error in loading Summoners")
+	}
+	return summs
+}
+
+func getLoLRunes(language string) LoLRunes {
+	var runes LoLRunes = LoLRunes{}
+	err := loadFileInStruct(generateLink(lolDataLinks.RunesLink, []string{lolDataVersion, language}), &runes)
+	if err != nil {
+		fmt.Println(err.Error())
+		panic("Error in loading Runes")
+	}
+	return runes
+}
+
 // data manager
 
 type LoLDataManager struct {
 	Languages      LoLLanguages
 	ChosenLanguage string
 	Items          LoLItems
+	Champions      LoLChampions
+	Summoners      LoLSummoners
+	Runes          LoLRunes
 }
 
 func NewLoLDataManager() LoLDataManager {
 	var lolDataManager LoLDataManager = LoLDataManager{}
 	lolDataManager.Languages = getLoLLanguages()
 	lolDataManager.ChosenLanguage = lolDataManager.Languages.getBaseLoLLanguage()
-	lolDataManager.Items = getLoLItems(lolDataManager.ChosenLanguage)
+	lolDataManager.updateData()
 	return lolDataManager
+}
+
+func (lolDataManager *LoLDataManager) updateData() {
+	lolDataManager.Items = getLoLItems(lolDataManager.ChosenLanguage)
+	lolDataManager.Champions = getLoLChampions(lolDataManager.ChosenLanguage)
+	lolDataManager.Summoners = getLoLSummoners(lolDataManager.ChosenLanguage)
+	lolDataManager.Runes = getLoLRunes(lolDataManager.ChosenLanguage)
 }
 
 // main
 
 func main() {
-	checkLoadings()
-
 	test := NewLoLDataManager()
-	full := test.Items.Data["1001"].Image.Full
-	fmt.Println(generateLink(lolDataLinks.ItemsFullImageLink, []string{lolDataVersion, full}))
 }
